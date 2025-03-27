00c99f258add32fcba0d7d8531f0910335f1e6bb
LucSEILER <lucky200331a@gmail.com>
2025-03-27 15:10:51 +0100

    feat: Store todo into the localStorage

--- src/app/controllers/todo.controllers.ts
+++ src/app/controllers/todo.controllers.ts
@@ -1,31 +1,48 @@
-
 import { Todo } from '../models/todo.models';
 
 export class TodoController {
   todos: Todo[] = [];
 
+  constructor() {
+    this.loadTodos();
+  }
+
   addTodo(task: string): void {
     const newTodo = new Todo(this.todos.length + 1, task);
     this.todos.push(newTodo);
+    this.saveTodos();
   }
 
   removeTodo(id: number): void {
-    this.todos = this.todos.filter(todo => todo.id !== id);
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    this.saveTodos();
   }
 
   toggleComplete(id: number): void {
-    const todo = this.todos.find(todo => todo.id === id);
+    const todo = this.todos.find((todo) => todo.id === id);
     if (todo) {
       todo.completed = !todo.completed;
+      this.saveTodos();
     }
   }
-  
+
   getTodos(filter: 'all' | 'completed' | 'pending'): Todo[] {
     if (filter === 'completed') {
-      return this.todos.filter(todo => todo.completed);
+      return this.todos.filter((todo) => todo.completed);
     } else if (filter === 'pending') {
-      return this.todos.filter(todo => !todo.completed);
+      return this.todos.filter((todo) => !todo.completed);
     }
-    return this.todos; 
+    return this.todos;
+  }
+
+  private saveTodos(): void {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  private loadTodos(): void {
+    const todos = localStorage.getItem('todos');
+    if (todos) {
+      this.todos = JSON.parse(todos);
+    }
   }
 }
